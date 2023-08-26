fdfeb8b59a7635ec1285237fcbbe5fef00009a42
Pedro Gontijo <70620630+PedroGontijo13@users.noreply.github.com>
2023-08-25 17:58:55 -0700

    ✅

--- app/store/store.js
+++ app/store/store.js
@@ -30,18 +30,23 @@ const useStore = create((set) => ({
     },
     decreaseCartItemQuantity: (item) => {
         set((state) => {
-            const existingItem = state.cart.find((cartItem) => cartItem.id === item.id)
-            if (existingItem && existingItem.quantity > 1) {
-                const updatedCartItems = state.cart.map((cartItem) => {
-                    if (cartItem.id === item.id) {
-                        return { ...cartItem, quantity: cartItem.quantity - 1 }
-                    }
-                    return cartItem
-                })
-                return { cart: updatedCartItems }
+            const existingItem = state.cart.find((cartItem) => cartItem.id === item.id);
+            if (existingItem) {
+                if (existingItem.quantity > 1) {
+                    const updatedCartItems = state.cart.map((cartItem) => {
+                        if (cartItem.id === item.id) {
+                            return { ...cartItem, quantity: cartItem.quantity - 1 };
+                        }
+                        return cartItem;
+                    });
+                    return { cart: updatedCartItems };
+                } else {
+                    const updatedCartItems = state.cart.filter((cartItem) => cartItem.id !== item.id);
+                    return { cart: updatedCartItems };
+                }
             }
-            return state
-        })
+            return state;
+        });
     },
     increaseCartItemQuantity: (item) => {
         set((state) => {
