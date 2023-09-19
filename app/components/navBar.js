e1e2456c3772cd75300a88c93e753443e05bf2e3
Pedro Gontijo <70620630+PedroGontijo13@users.noreply.github.com>
2023-09-19 15:56:52 -0700

    ✅

--- app/components/navBar.js
+++ app/components/navBar.js
@@ -27,18 +27,15 @@ export default function NavBar() {
                         </button>
                     </div>
                     <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-                        <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <ul class="flex flex-col md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
-                                <a href="/" className={`block py-2 pl-3 pr-4 ${pathname === '/' ? 'text-blue-700 md:text-blue-700 md:bg-transparent' : 'text-white md:text-white'} rounded md:p-0`} aria-current="page">Home</a>
+                                <a href="/" className={`block py-2 pl-3 pr-4 ${pathname === '/' ? 'text-blue-700 md:text-blue-700 md:bg-transparent' : 'text-white md:text-white'} rounded md:p-0 mx-10`} aria-current="page">Home</a>
                             </li>
                             <li>
-                                <a href="/about" className={`block py-2 pl-3 pr-4 ${pathname === '/about' ? 'text-blue-700 md:text-blue-700 md:bg-transparent' : 'text-white'} rounded md:p-0`} >About</a>
+                                <a href="/about" className={`block py-2 pl-3 pr-4 ${pathname === '/about' ? 'text-blue-700 md:text-blue-700 md:bg-transparent' : 'text-white'} rounded md:p-0 mx-10`} >About</a>
                             </li>
                             <li>
-                                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                                <a href="/contact" class={`block py-2 pl-3 pr-4 ${pathname === '/contact' ? 'text-blue-700 md:text-blue-700 md:bg-transparent' : 'text-white'} rounded md:p-0 mx-10`}>Contact</a>
                             </li>
                         </ul>
                     </div>
