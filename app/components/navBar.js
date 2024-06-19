b1388629c6e0307b98f80dcbe80281bb5a9cf30a
Pedro Gontijo <pedrotolentino2002@gmail.com>
2024-06-19 11:52:58 -0300

    NavBarChange

--- app/components/navBar.js
+++ app/components/navBar.js
@@ -1,45 +1,41 @@
-"use client"
-import React from "react"
-import { BsCart } from 'react-icons/bs'
-import useStore from "../store/store.js"
-import { usePathname, useRouter } from "next/navigation.js"
+"use client";
+import React, { useEffect } from "react";
+import cartSvg from "../../public/cart.svg";
+import useStore from "../store/store.js";
+import Image from "next/image.js";
 
 export default function NavBar() {
-    const setModalVisible = useStore((state) => state.setModalVisible);
-    const pathname = usePathname()
+  const setModalVisible = useStore((state) => state.setModalVisible);
+  const getTotalItems = useStore((state) => state.getTotalItems);
+  const cart = useStore((state) => state.cart)
+  useEffect(() => {
+    console.log(getTotalItems());
+  }, [getTotalItems, cart]);
 
-    return (
-        <header>
-            <nav class="bg-white border-gray-200 dark:bg-gray-900">
-                <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <a href="/" class="flex items-center">
-                        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
-                        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MyStore</span>
-                    </a>
-                    <div class="flex md:order-2">
-                        <BsCart className="h-10 justify-center text-black dark:text-gray-300" onClick={setModalVisible} />
-                        <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
-                            <span class="sr-only">Open main menu</span>
-                            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
-                            </svg>
-                        </button>
-                    </div>
-                    <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-                        <ul class="flex flex-col md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                            <li>
-                                <a href="/" className={`block py-2 pl-3 pr-4 ${pathname === '/' ? 'text-blue-700 md:text-blue-700 md:bg-transparent' : 'text-white md:text-white'} rounded md:p-0 mx-10`} aria-current="page">Home</a>
-                            </li>
-                            <li>
-                                <a href="/about" className={`block py-2 pl-3 pr-4 ${pathname === '/about' ? 'text-blue-700 md:text-blue-700 md:bg-transparent' : 'text-white'} rounded md:p-0 mx-10`} >About</a>
-                            </li>
-                            <li>
-                                <a href="/contact" class={`block py-2 pl-3 pr-4 ${pathname === '/contact' ? 'text-blue-700 md:text-blue-700 md:bg-transparent' : 'text-white'} rounded md:p-0 mx-10`}>Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
-    )
+  return (
+    <header>
+      <nav class="bg-white border-gray-200 dark:bg-gray-900">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="/" class="flex items-center">
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-8 mr-3"
+              alt="Flowbite Logo"
+            />
+          </a>
+          <div class="flex md:order-2 relative">
+            <Image
+              src={cartSvg}
+              alt="cart-logo"
+              className="h-10 justify-center text-black dark:text-gray-300"
+              onClick={setModalVisible}
+            />
+            <span className="absolute right-0 top-0 transform translate-x-1/2 -translate-y-1/2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center">
+              {getTotalItems()}
+            </span>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
--- app/store/store.js
+++ app/store/store.js
@@ -1,91 +1,96 @@
 import { create } from "zustand";
 
-const useStore = create((set) => ({
-    products: [],
-    cart: [],
-    product: {},
-    modalVisible: false,
-    fullName: '',
-    email: '',
-    subject: '',
-    message: '',
-    setEmail: (email) => set({email}),
-    setSubject: (subject) => set({subject}),
-    setMessage: (message) => set({message}),
-    setFullName: (fullName) => set({fullName}),
-    setModalVisible: () => set({ modalVisible: true }),
-    setModalVisibleFalse: () => set({ modalVisible: false }),
-    addProducts: (productsStripe) => set((state) => ({ products: [...state.products, ...productsStripe] })),
-    setProduct: (newProduct) => {
-        set((state) => ({
-            ...state,
-            product: newProduct
-        }));
-    },
-    addItemToCart: (item) => {
-        set((state) => {
-            const existingItemIndex = state.cart.findIndex((cartItem) => cartItem.id === item.id);
-            console.log("Existing Item Index:", existingItemIndex);
-
-            if (existingItemIndex !== -1) {
-                console.log("Item already exists in cart.");
-                const updatedCartItems = state.cart.map((cartItem, index) => {
-                    if (index === existingItemIndex) {
-                        return { ...cartItem, quantity: cartItem.quantity + 1 };
-                    }
-                    return cartItem;
-                });
-                console.log("Updated Cart:", updatedCartItems);
-                return { cart: updatedCartItems };
-            } else {
-                console.log("Item does not exist in cart.");
-                return { cart: [...state.cart, { ...item, quantity: 1 }] };
+const useStore = create((set, get) => ({
+  products: [],
+  cart: [],
+  product: {},
+  modalVisible: false,
+  fullName: "",
+  email: "",
+  subject: "",
+  message: "",
+  setEmail: (email) => set({ email }),
+  setSubject: (subject) => set({ subject }),
+  setMessage: (message) => set({ message }),
+  setFullName: (fullName) => set({ fullName }),
+  setModalVisible: () => set({ modalVisible: true }),
+  setModalVisibleFalse: () => set({ modalVisible: false }),
+  addProducts: (productsStripe) =>
+    set((state) => ({ products: [...state.products, ...productsStripe] })),
+  setProduct: (newProduct) => {
+    set((state) => ({
+      ...state,
+      product: newProduct,
+    }));
+  },
+  addItemToCart: (item) => {
+    set((state) => {
+      const existingItemIndex = state.cart.findIndex(
+        (cartItem) => cartItem.id === item.id
+      );
+      if (existingItemIndex !== -1) {
+        const updatedCartItems = state.cart.map((cartItem, index) => {
+          if (index === existingItemIndex) {
+            return { ...cartItem, quantity: cartItem.quantity + 1 };
+          }
+          return cartItem;
+        });
+        return { cart: updatedCartItems };
+      } else {
+        return { cart: [...state.cart, { ...item, quantity: 1 }] };
+      }
+    });
+  },
+  decreaseCartItemQuantity: (item) => {
+    set((state) => {
+      const existingItem = state.cart.find(
+        (cartItem) => cartItem.id === item.id
+      );
+      if (existingItem) {
+        if (existingItem.quantity > 1) {
+          const updatedCartItems = state.cart.map((cartItem) => {
+            if (cartItem.id === item.id) {
+              return { ...cartItem, quantity: cartItem.quantity - 1 };
             }
-        });
-    },
-    decreaseCartItemQuantity: (item) => {
-        set((state) => {
-            const existingItem = state.cart.find((cartItem) => cartItem.id === item.id);
-            if (existingItem) {
-                if (existingItem.quantity > 1) {
-                    const updatedCartItems = state.cart.map((cartItem) => {
-                        if (cartItem.id === item.id) {
-                            return { ...cartItem, quantity: cartItem.quantity - 1 };
-                        }
-                        return cartItem;
-                    });
-                    return { cart: updatedCartItems };
-                } else {
-                    const updatedCartItems = state.cart.filter((cartItem) => cartItem.id !== item.id);
-                    return { cart: updatedCartItems };
-                }
-            }
-            return state;
-        });
-    },
-    increaseCartItemQuantity: (item) => {
-        set((state) => {
-            const updatedCartItems = state.cart.map((cartItem) => {
-                if (cartItem.id === item.id) {
-                    return { ...cartItem, quantity: cartItem.quantity + 1 }
-                }
-                return cartItem
-            })
-            return { cart: updatedCartItems }
-        })
-    },
-    removeItemFromCart: (params) => {
-        const { itemIndex } = params;
-        set((state) => {
-            const newCart = state.cart.filter((element, elementIndex) => {
-                return elementIndex !== itemIndex;
-            });
-            return {
-                ...state,
-                cart: newCart,
-            };
-        });
-    },
+            return cartItem;
+          });
+          return { cart: updatedCartItems };
+        } else {
+          const updatedCartItems = state.cart.filter(
+            (cartItem) => cartItem.id !== item.id
+          );
+          return { cart: updatedCartItems };
+        }
+      }
+      return state;
+    });
+  },
+  increaseCartItemQuantity: (item) => {
+    set((state) => {
+      const updatedCartItems = state.cart.map((cartItem) => {
+        if (cartItem.id === item.id) {
+          return { ...cartItem, quantity: cartItem.quantity + 1 };
+        }
+        return cartItem;
+      });
+      return { cart: updatedCartItems };
+    });
+  },
+  removeItemFromCart: (params) => {
+    const { itemIndex } = params;
+    set((state) => {
+      const newCart = state.cart.filter((element, elementIndex) => {
+        return elementIndex !== itemIndex;
+      });
+      return {
+        ...state,
+        cart: newCart,
+      };
+    });
+  },
+  getTotalItems: () => {
+    return get().cart.reduce((total, item) => total + item.quantity, 0);
+  },
 }));
 
 export default useStore;
